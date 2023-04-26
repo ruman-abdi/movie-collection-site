0a6b490738604592e07de60ef04d04d7bb5e0e80
ruman-abdi <rahrumanabdii@gmail.com>
2023-04-25 21:26:47 -0700

    search input functionalty set uped

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.css'
+import searchicon from'./search.svg';
+import MovieCard from './MovieCard';
+import { useEffect ,useState} from "react";
+
+
+const  API= "http://www.omdbapi.com?apikey=52c62190"
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const [searchTerm, setSearchTerm] = useState('')
+const [movies,setMovies] = useState([])
+const [showModal, setShowModal] = useState(false);
 
+
+
+const searchMovies = async (title) => {
+  const response = await fetch(`${API}&s=${title}`);
+  const data = await response.json();
+  if (data.Response === 'False') {
+    setShowModal(true);
+    setMovies([]);
+  } else {
+    setMovies(data.Search);
+    setShowModal(false);
+  }
+};
+
+
+useEffect(() => {
+  searchMovies('spiderman');
+}, []);
+
+const handleSearch = () => {
+  searchMovies(searchTerm);
+};
+
+const handleModalClose = () => {
+  setShowModal(false);
+};
+  
+return (
+  <div className="App">
+    <h1>Movie Land</h1>
+
+    <div className="search">
+      <input
+        type="text"
+        placeholder="Search movies..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+
+
+      <img src={searchicon} alt="Search" onClick={handleSearch} />
+      </div>
+
+      <div className="container">
+        {movies.map((movie) => (
+          <MovieCard key={movie.imdbID} data={movie} />
+        ))}
+      </div>
+
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <p>Movie not found.</p>
+            <button onClick={handleModalClose}>Close</button>
+          </div>
+        </div>
+      )}
+
+      <div className={`container ${showModal ? 'hidden' : ''}`}>
+        {movies.map((movie) => (
+          <MovieCard key={movie.imdbID} data={movie} />
+        ))}
+      </div>
+  </div>
+    
+)}
+    
 export default App;
---
+++ src/MovieCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const MovieCard = (props) => {
+  
+  const{Year, Poster, Type, Title} = props.data;
+
+  return (
+    <div>
+    <div className='movie'>
+    <div>  
+            <p>{Year}</p>
+        </div>
+     <div>
+     <img src={Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"} alt={Title} />
+         
+     </div>
+   
+     <div>
+     <span>{Type}</span>
+     <h3>{Title}</h3>
+     </div>
+    </div>
+    </div>
+  )
+}
+
+export default MovieCard
